e83ae1faf7166f69efcf8f6505e1e8acc872e309
Julian Kornberger <jk+github@digineo.de>
2019-01-20 11:52:42 +0100

    Try to login again on status 401

--- exporter/api.go
+++ exporter/api.go
@@ -3,6 +3,7 @@ package exporter
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -19,6 +20,9 @@ type API struct {
 	Password string
 	client   *http.Client
 }
+
+// ErrAuthentication is returned on HTTP status 401
+var ErrAuthentication = errors.New("authentication failed")
 
 // Login creates a user session
 func (api *API) Login() error {
@@ -61,19 +65,38 @@ func (api *API) buildURI(path string) string {
 
 // getResponse does a GET request and parses the JSON response
 func (api *API) getResponse(path string, response interface{}) error {
+	retried := false
+
+request:
 	resp, err := api.client.Get(api.buildURI(path))
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
 
-	if resp.StatusCode != 200 {
+	switch resp.StatusCode {
+	case 200:
+		if contentType := resp.Header.Get("Content-Type"); !strings.HasPrefix(contentType, "application/json") {
+			return fmt.Errorf("unexpected content-type: %s", contentType)
+		}
+
+		return json.NewDecoder(resp.Body).Decode(response)
+	case 401:
+		// session expired
+		if retried {
+			return ErrAuthentication
+		}
+
+		// try to login again
+		err = api.Login()
+		if err != nil {
+			return err
+		}
+
+		// login successful, retry request
+		retried = true
+		goto request
+	default:
 		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
 	}
-
-	if contentType := resp.Header.Get("Content-Type"); !strings.HasPrefix(contentType, "application/json") {
-		return fmt.Errorf("unexpected content-type: %s", contentType)
-	}
-
-	return json.NewDecoder(resp.Body).Decode(response)
 }
