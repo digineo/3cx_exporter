e026aa07ac4339a124d55eb40478c54e8bb38ba0
denisskripnik <valis.source@gmail.com>
2022-09-23 09:38:52 +0300

    http grasefull shutdown

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"flag"
-	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/digineo/3cx_exporter/exporter"
 	"github.com/prometheus/client_golang/prometheus"
@@ -16,16 +19,31 @@ func main() {
 	flag.Parse()
 
 	api, err := parseConfig(*config)
-	checkErr(err)
-	checkErr(api.Login())
-
-	prometheus.MustRegister(&exporter.Exporter{API: *api})
-	http.Handle("/metrics", promhttp.Handler())
-	log.Fatal(http.ListenAndServe(*listen, nil))
-}
-
-func checkErr(err error) {
 	if err != nil {
 		panic(err)
 	}
+	if err := api.Login(); err != nil {
+		panic(err)
+	}
+	//Ragister metrics
+	prometheus.MustRegister(&exporter.Exporter{API: *api})
+	srv := &http.Server{
+		Addr: *listen,
+	}
+	http.Handle("/metrics", promhttp.Handler())
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			panic(err)
+		}
+	}()
+
+	// Listen for os sygnals
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*60)
+	defer cancel()
+	srv.Shutdown(ctx)
+	os.Exit(0)
 }
